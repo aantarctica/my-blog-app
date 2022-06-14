9d314068a278d6425850c4fed16a644324ce2b0d
Mutya <mcgulapa@up.edu.ph>
2022-06-14 17:01:23 +0800

    Login Page UI

--- pages/login.vue
+++ pages/login.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="app-container">
-    <div
-      class="
-        flex flex-col flex-nowrap
-        justify-around
-        gap-4
-        bg-gray-100
-        p-10
-        rounded-lg
-      "
-    >
-      <div class="text-lg font-bold">Login</div>
+  <div class="app-container text-gray-800">
+    <div class="flex-col-center gap-4 bg-gray-100 p-10 rounded-lg">
+      <div class="text-2xl font-bold">Login</div>
       <form class="flex flex-col gap-2">
-        <label for="email">Email</label>
+        <label for="email" class="font-bold">Email</label>
         <input
           type="text"
           class="bg-gray-200 rounded-md h-8 px-3"
           placeholder="Email"
         />
-      </form>
-      <form class="flex flex-col gap-2">
-        <label for="password">Password</label>
+        <label for="password" class="font-bold">Password</label>
         <input
           type="password"
           class="bg-gray-200 rounded-md h-8 px-3"
           placeholder="Password"
         />
+        <button
+          type="submit"
+          class="
+            bg-green-500
+            hover:bg-green-700
+            text-white
+            font-bold
+            py-1
+            px-2
+            rounded-md
+            mt-6
+          "
+        >
+          Submit
+        </button>
       </form>
     </div>
   </div>
