a14c4c15bc1063f1d0d6d70ad83c0d8fb7a80a28
Mutya <mcgulapa@up.edu.ph>
2022-06-16 02:04:42 +0800

    Added component for blog posts

---
+++ components/BlogPosts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <section>{{ this.$store.state.articleList[0].title }}</section> -->
+    helo
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {},
+};
+</script>
